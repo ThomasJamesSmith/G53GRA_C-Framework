246df777f25eef42175b5314d12e20352a53427c
Wil O. C. Ward <wcw@cs.nott.ac.uk>
2016-02-08 14:28:33 +0000

    Initial Demos
    G53GRA Coursework Framework for Visual Studio and XCode

--- G53GRA.Framework.MSVS/G53GRA.Framework.MSVS/Code/MyScene.cpp
+++ G53GRA.Framework.MSVS/G53GRA.Framework.MSVS/Code/MyScene.cpp
@@ -1,5 +1,9 @@
 #include "MyScene.h"
+
 #include "Triangle.h"
+#include "Triforce.h"
+#include "Planet.h"
+#include "Planet2.h"
 
 MyScene::MyScene(int argc, char** argv, const char *title, const int& windowWidth, const int& windowHeight)
 	: Scene(argc, argv, title, windowWidth, windowHeight)
@@ -11,8 +15,24 @@ void MyScene::Initialise()
 {
 	glClearColor(0.f, 0.f, 0.f, 1.f);
 
-	Triangle *t = new Triangle();
-	AddObjectToScene(t);
+	//Triangle *t = new Triangle();
+    //Triforce *t = new Triforce();
+    
+    // AddObjectToScene(t);
+    
+    // create and add a new triangle to the scene
+    Planet *sun = new Planet(30.0f, 0.0f, 12.0f, 0.0f);
+    sun->SetColour(255, 255, 0); //yellow
+    
+    Planet *mars = new Planet(6.0f, 60.0f, 150.0f, 50.0f);
+    mars->SetColour(255, 0, 0); //red
+    
+    Planet2 *earth = new Planet2(15.0f, 135.0f, 100.0f, 20.0f, 6.0f, 30.0, 100.0f, 8.0f );
+    earth->SetColour(0, 0, 255); //earth blue, moon grey
+    
+    AddObjectToScene(sun);
+    AddObjectToScene(mars);
+    AddObjectToScene(earth);
 }
 
 void MyScene::Projection()
